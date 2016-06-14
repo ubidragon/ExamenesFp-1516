3be7f5526f7e42f8cc2cb2ce1fe838e450e874de
ubidragon <ubahidarr@alum.us.es>
2016-06-14 08:25:50 +0200

    Primer Parcial - Ejercicio 3

---
+++ Primer_Parcial/src/fp/formula1/tipos/MundiaF1.java
@@ -0,0 +1,18 @@
+package fp.formula1.tipos;
+
+import java.util.SortedSet;
+import java.util.Set;
+
+public interface MundialF1 {
+// Propiedades
+	Integer getAño();
+	SortedSet<CarreraF1> getCalendario();
+	Set<PilotoF1Puntuado> getPilotos();
+// Operaciones
+	List<CarreraF1> posterioresA(CarreraF1 c);
+	void puntuarCarrera(CarreraF1 c);
+	Integer carrerasGanadas(PilotoF1 p);
+	String masCarrerasGanadas();
+	PilotoF1 ganaEnCasa();
+	Boolean existeGanador(String pais, Integer n);
+}
