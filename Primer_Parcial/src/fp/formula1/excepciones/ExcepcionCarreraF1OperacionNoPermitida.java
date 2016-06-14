4c33c05d09dbcad73a3ef075d255a50b8fdbaa0a
ubidragon <ubahidarr@alum.us.es>
2016-06-14 08:52:27 +0200

    Creaciones de Excepciones

---
+++ Primer_Parcial/src/fp/formula1/excepciones/ExcepcionCarreraF1OperacionNoPermitida.java
@@ -0,0 +1,19 @@
+package src.fp.formula1.excepciones;
+
+public class ExcepcionCarreraF1OperacionNoPermitida extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ExcepcionCarreraF1OperacionNoPermitida() {
+
+		super();
+
+	}
+
+	public ExcepcionCarreraF1OperacionNoPermitida(String string) {
+
+		super(string);
+
+	}
+
+}
---
+++ Primer_Parcial/src/fp/formula1/excepciones/ExcepcionPilotoF1PuntuadoOperacionNoPermitida.java
@@ -0,0 +1,19 @@
+package src.fp.formula1.excepciones;
+
+public class ExcepcionPilotoF1PuntuadoOperacionNoPermitida extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ExcepcionPilotoF1PuntuadoOperacionNoPermitida() {
+
+		super();
+
+	}
+
+	public ExcepcionPilotoF1PuntuadoOperacionNoPermitida(String string) {
+
+		super(string);
+
+	}
+
+}
